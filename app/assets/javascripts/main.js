df11bf3587979f0b5f68203b31f7c45d2ccc4c24
nik <worldnillin@gmail.com>
2017-08-14 17:02:10 +0300

    yandex_metrika

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -133,3 +133,32 @@ $(window).on("load",function(){
     $('header .line').fadeIn();
     $('header .fade').animate({opacity: 1},500);
 });
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter45616473 = new Ya.Metrika({
+                    id:45616473,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/45616473" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.gem "yandex_metrika", :lib => "yandex/metrika", :source => "http://gemcutter.org"
-
-if defined?Yandex::Metrika
-  Yandex::Metrika.counter_id = '45616473'
-end
