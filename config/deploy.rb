111b0dc601ad9d521d0be4ce80248fbaa99a747e
nik <worldnillin@gmail.com>
2017-08-15 02:09:29 +0300

    dotenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :logtail_lines, 50
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs,  "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", "public/uploads"
